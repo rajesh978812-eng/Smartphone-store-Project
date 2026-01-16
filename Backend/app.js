b2e089befc0039a0865a6065a31ed885cb806a93
RAJESH S <rajesh978812@gmail.com>
2026-01-16 08:06:47 +0530

    Refactor dotenv and update server port logic
    Refactor dotenv configuration and server port handling.

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,11 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
-const path = require('path');
+
+
 const cors = require('cors');
 const connectDatabase = require('./config/connectDatabase');
-dotenv.config({path: path.join(__dirname, 'config', 'config.env')})
+
 
 const products = require('./routes/product');
 const orders = require('./routes/order');
@@ -16,6 +17,8 @@ app.use(cors());
 app.use('/api/v1/',products);
 app.use('/api/v1/',orders);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`)
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening to Port ${PORT}`);
 });
